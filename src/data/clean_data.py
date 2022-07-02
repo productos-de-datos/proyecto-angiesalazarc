b117ed4d713bff08f62323b3d7712d1db0ed2cce
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-07-02 17:59:28 -0500

    clean_data

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -1,5 +1,4 @@
-def clean_data():
-    """Realice la limpieza y transformación de los archivos CSV.
+"""Realice la limpieza y transformación de los archivos CSV.
 
     Usando los archivos data_lake/raw/*.csv, cree el archivo data_lake/cleansed/precios-horarios.csv.
     Las columnas de este archivo son:
@@ -11,11 +10,33 @@ def clean_data():
     Este archivo contiene toda la información del 1997 a 2021.
 
 
-    """
+"""
+
+import pandas as pd
+import glob
+
+def clean_data():
+
+    ruta = glob.glob(r'data_lake/raw/*.csv')
+
+    lista_data = []
+    for file in ruta:
+        df = pd.read_csv(file, index_col=None, header=0)
+        lista_data.append(df)
+
+    data_completo = pd.concat(lista_data, axis=0, ignore_index=True)
+    data_completo = data_completo[data_completo["Fecha"].notnull()]
+
+    archivo = pd.melt(data_completo, id_vars = ["Fecha"])
+    archivo = archivo.rename(columns = {"Fecha": "fecha", "variable": "hora", "value": "precio"})
+    archivo = archivo[archivo["precio"].notnull()]
+
+    archivo.to_csv("data_lake/cleansed/precios-horarios.csv", index=None, header = True )
+
     raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
-
+    clean_data()
     doctest.testmod()
--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -11,14 +11,26 @@ def transform_data():
     """
     for fec in range(1995, 2022):
 
-        if fec in range(2016, 2018):
+        if fec in range(1995, 2000):
+            datos_csv = pd.read_excel('data_lake/landing/{}.xlsx'.format(fec), header=3)
+            datos_csv = datos_csv.iloc[:, 0:25]
+            datos_csv.columns = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10','H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
+            datos_csv.to_csv ('data_lake/raw/{}.csv'.format(fec), index=None)
+        
+        elif fec in range(2000, 2016):
+            datos_csv = pd.read_excel('data_lake/landing/{}.xlsx'.format(fec), header=2)
+            datos_csv = datos_csv.iloc[:, 0:25]
+            datos_csv.columns = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10','H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
+            datos_csv.to_csv ('data_lake/raw/{}.csv'.format(fec), index=None)
+
+        elif fec in range(2016, 2018):
             datos_csv = pd.read_excel('data_lake/landing/{}.xls'.format(fec), header=2)
             datos_csv = datos_csv.iloc[:, 0:25]
             datos_csv.columns = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10','H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
             datos_csv.to_csv ('data_lake/raw/{}.csv'.format(fec), index=None)
 
         else:
-            datos_csv = pd.read_excel('data_lake/landing/{}.xlsx'.format(fec), header=2)
+            datos_csv = pd.read_excel('data_lake/landing/{}.xlsx'.format(fec), header=0)
             datos_csv = datos_csv.iloc[:, 0:25]
             datos_csv.columns = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10','H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
             datos_csv.to_csv ('data_lake/raw/{}.csv'.format(fec), index=None)
