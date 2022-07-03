2d0f11748bdc796086efe1755d2d914070110416
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-07-03 17:03:30 -0500

    Actualizacion

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -20,13 +20,39 @@ import luigi
 
 from luigi import Task, LocalTarget
 
+class IngestTransformClean(Task):
+    def output(self):
+        return LocalTarget("data_lake/cleansed/precios-horarios.csv")
+
+    def run(self):
+        ingest_data.ingest_data()
+        transform_data.transform_data()
+        clean_data.clean_data()
+
+class Computes(Task):
+    def requires(self):
+        return IngestTransformClean()
+
+    def output(self):
+        return LocalTarget(
+            [
+                "data_lake/business/precios-diarios.csv",
+                "data_lake/business/precios-mensuales.csv"
+            ]
+        )
+
+    def run(self):
+        compute_daily_prices.compute_daily_prices()
+        compute_monthly_prices.compute_monthly_prices()
 
 
-if __name__ == "__main__":
+#if __name__ == "__main__":
 
-    raise NotImplementedError("Implementar esta función")
+    #raise NotImplementedError("Implementar esta función")
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+    
+    luigi.run(["Computes", "--local-scheduler"])
