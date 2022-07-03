eabc23e5fccae9d7b3703e584388c28f4946fd2a
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-07-03 18:04:33 -0500

    Actualizacion

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
 def make_daily_prices_plot():
     """Crea un grafico de lines que representa los precios promedios diarios.
 
@@ -7,10 +10,30 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+
+    data = r'data_lake/business/precios-diarios.csv'
+    datos = pd.read_csv(data, index_col=None, sep=',', header=0)
+    datos["fecha"] = pd.to_datetime(datos["fecha"])
+
+    x=datos.fecha
+    y=datos.precio
+
+    plt.figure(figsize=(15, 6))
+    plt.plot(x, y, 'b', label='Promedio diario')
+    plt.title('Promedio diario')
+    plt.xlabel('Fecha')
+    plt.ylabel('Precio')
+    plt.legend()
+    plt.xticks(rotation="vertical")
+    plt.savefig('data_lake/business/reports/figures/daily_prices.png')
+
+
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+
+    make_daily_prices_plot()
