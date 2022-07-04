3f0ab9c7fe9373bea1bd91862aaf01b790305207
angiesalazarc <101910520+angiesalazarc@users.noreply.github.com>
2022-07-03 22:58:14 -0500

    Actualizacion

--- src/models/train_daily_model.py
+++ src/models/train_daily_model.py
@@ -6,10 +6,38 @@ def train_daily_model():
 
 
     """
-    raise NotImplementedError("Implementar esta función")
+
+    import pandas as pd
+    import pickle
+    import numpy as np
+    from sklearn.linear_model import LinearRegression
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import mean_squared_error
+    from sklearn.ensemble import RandomForestRegressor
+
+    df = pd.read_csv("data_lake/business/features/precios-diarios.csv", encoding = 'utf-8', sep=',')
+
+    df["fecha"] = pd.to_datetime(df["fecha"]).dt.strftime('%Y%m%d')
+    X_fecha = np.array(df['fecha']).reshape(-1,1)
+
+    y_precio = np.array(df['precio']).reshape(-1,1)
+
+    (X_train, X_test, y_train, y_test,) = train_test_split(X_fecha, y_precio, test_size=0.2, random_state=123456,)
+
+    clf = RandomForestRegressor(n_estimators=100, max_features='sqrt', n_jobs=-1, oob_score = True, random_state = 123456)
+    
+    clf.fit(X_train,y_train)
+
+    pickle.dump(clf, open('src/models/precios-diarios.pickle', 'wb'))
+    
+    #raise NotImplementedError("Implementar esta función")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+
+    train_daily_model()
+
+
